132448b9babe4a35efe75600841627ce67be880c
Matt Bartholomew <matthew.b.bartholomew@gmail.com>
2022-11-28 14:03:30 -0500

    new statement

--- testchild.py
+++ testchild.py
@@ -1,3 +1,4 @@
 ## Adding a new file in a child branch
 print("Inside Child Branch")
 print("New print statement for accessing git from local machine")
+print("New print statement for testing git access from work machine")
